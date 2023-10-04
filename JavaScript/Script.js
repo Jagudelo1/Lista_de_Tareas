4a36e40ff275212ce4a2ae5d00988de63a1864b3
Juan David Agudelo <93266129+Jagudelo1@users.noreply.github.com>
2023-10-04 18:20:37 -0500

    Delete

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,78 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const addTaskButton = document.getElementById("addTaskButton");
-    const editTaskModal = new bootstrap.Modal(document.getElementById("editTaskModal"));
-    const deleteTaskModal = new bootstrap.Modal(document.getElementById("deleteTaskModal"));
-    const saveEditButton = document.getElementById("saveEditButton");
-    const confirmDeleteButton = document.getElementById("confirmDeleteButton");
+// Función para realizar la solicitud a la API de SWAPI
+function fetchCharacters() {
+    const apiUrl = 'https://swapi.dev/api/people/';
 
-    addTaskButton.addEventListener("click", function() {
-        const taskText = document.getElementById("taskInput").value;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const characterList = document.getElementById('character-list');
 
-        if (taskText.trim() !== "") {
-            const taskContainer = document.createElement("div");
-            taskContainer.classList.add("task-container");
-            
-            //Contenedor del checkbox y el span
-            const contentText = document.createElement("div");
-            contentText.classList.add("content-text");
-
-            //Input checkbox
-            const checkbox = document.createElement("input");
-            checkbox.className = "InputCheckbox"
-            checkbox.type = "checkbox";
-
-            //Span
-            const taskTextElement = document.createElement("span");
-            taskTextElement.className = "Span";
-            taskTextElement.textContent = taskText;
-
-            //Contenedor de los botones
-            const contentButtons = document.createElement("div");
-            contentButtons.classList.add("content-buttons");
-
-            //Botón de Editar
-            const editButton = document.createElement("button");
-            editButton.textContent = "Editar";
-            editButton.className = "ButtonEditar";
-            editButton.classList.add("btn", "btn-primary", "btn-sm");
-
-            //Botón de Eliminar
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent = "Eliminar";
-            editButton.className = "ButtonEliminar";
-            deleteButton.classList.add("btn", "btn-danger", "btn-sm");
-
-            contentText.appendChild(checkbox);
-            contentText.appendChild(taskTextElement);
-            contentButtons.appendChild(editButton);
-            contentButtons.appendChild(deleteButton);
-
-            taskContainer.appendChild(contentText);
-            taskContainer.appendChild(contentButtons);
-
-            document.getElementById("todo-list").appendChild(taskContainer);
-            document.getElementById("taskInput").value = "";
-
-            editButton.addEventListener("click", function() {
-                const newText = taskTextElement.textContent;
-                document.getElementById("editedTaskText").value = newText;
-                editTaskModal.show();
-
-                saveEditButton.addEventListener("click", function() {
-                    const editedText = document.getElementById("editedTaskText").value;
-                    taskTextElement.textContent = editedText;
-                    editTaskModal.hide();
-                });
+            // Iterar a través de los resultados y mostrarlos en la lista
+            data.results.forEach(character => {
+                const listItem = document.createElement('li');
+                listItem.innerHTML = `<strong>Nombre:</strong> ${character.name} </br><strong>Altura: </strong> ${character.height}`;
+                characterList.appendChild(listItem);
             });
+        })
+        .catch(error => {
+            console.error('Hubo un error al obtener los datos:', error);
+        });
+}
 
-            deleteButton.addEventListener("click", function() {
-                deleteTaskModal.show();
-
-                confirmDeleteButton.addEventListener("click", function() {
-                    taskContainer.remove();
-                    deleteTaskModal.hide();
-                });
-            });
-        }
-    });
-});
+// Llamar a la función para obtener los personajes
+fetchCharacters();
